3f754debf37ce46310bb7796958b725aacb1379d
Dekel Zoaretz <dekelzoaretz@gmail.com>
2021-10-28 08:58:24 +0300

    Create memoize.js

---
+++ memoize.js
@@ -0,0 +1,14 @@
+const memoize = (func, resolver) => {
+    const cache = new Map();
+    return (...args) => {
+        const key = resolver ? resolver.apply(this, args) : args[0];
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = func.apply(this, args);
+        cache.set(key, result);
+        return result;
+    }
+};
